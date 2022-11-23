f460d0b0b0bf0972de720f2355b17557da2d17f9
문효섭 <gytjq0723@naver.com>
2022-11-23 22:43:24 +0900

    parcel-bundler

---
+++ .babelrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env'
+  ],
+  Plugin:[
+    ['@babel/plugin-transform-runtime']
+  ]
+}
---
+++ .postcssrc.js
@@ -0,0 +1,12 @@
+//ESM
+//CommonJs
+
+//import autoprefixer from 'autoprefixer'
+const autoprefixer = require('autoprefixer')
+
+//export
+module.exports = {
+  plugins: [
+    autoprefixer
+  ]
+}
